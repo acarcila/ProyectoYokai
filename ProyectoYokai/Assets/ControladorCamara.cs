082248d7813b81a628e5c266b30257abc4ea869e
acarcila <acarcila98@gmail.com>
2018-09-02 10:05:37 -0500

    Cración personaje
    Se creó el personaje principal en primitivas (plano) y se modificó la cámara para seguirlo

---
+++ ProyectoYokai/Assets/ControladorCamara.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ControladorCamara : MonoBehaviour {
+
+	public GameObject objetivo;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		Vector3 posicionObjetivo = new Vector3 (objetivo.transform.position.x, objetivo.transform.position.y, transform.position.z);
+		transform.position = Vector3.MoveTowards (transform.position, posicionObjetivo, Time.deltaTime * 20f);
+//		transform.position = new Vector3 (objetivo.transform.position.x, objetivo.transform.position.y, transform.position.z);
+	}
+}
