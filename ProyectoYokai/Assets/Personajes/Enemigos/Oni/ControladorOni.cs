0a73b33ba79db69f3605195b3ee6d400057288e0
acarcila <acarcila98@gmail.com>
2018-09-27 16:47:17 -0500

    Se inició la creación del enemigo Oni

---
+++ ProyectoYokai/Assets/Personajes/Enemigos/Oni/ControladorOni.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ControladorOni : MonoBehaviour {
+
+     public GameObject objetivo;
+    public float velocidad;
+    
+	// Use this for initialization
+	void Start () {		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        transform.position = Vector3.MoveTowards(transform.position, objetivo.transform.position, velocidad * Time.deltaTime);
+	}
+}
