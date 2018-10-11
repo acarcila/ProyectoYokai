f82788894fffc039f163269c4850439ed1021dd3
acarcila <acarcila98@gmail.com>
2018-10-10 23:54:39 -0500

    Se creó un script para controlar la salud de los enemigos en general

---
+++ ProyectoYokai/Assets/Scripts/ControladorEnemigoVida.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ControladorEnemigoVida : MonoBehaviour {
+
+	public int vida;
+
+	public void reducirVida(int daño)
+	{
+		vida -= daño;
+
+		if(vida <= 0)
+		{
+			morir();
+		}
+	}
+
+    private void morir()
+    {
+        Destroy(this.gameObject);
+    }
+}
