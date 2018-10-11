0d65df41e7cbe90dfd0e8c804d3dcb9910c76398
acarcila <acarcila98@gmail.com>
2018-10-10 23:53:38 -0500

    Se creó el game manager

---
+++ ProyectoYokai/Assets/Scripts/GameManager.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour {
+
+	public GameObject oni;
+	public GameObject personaje;
+	private ControladorOni controladorOni;
+
+	// Use this for initialization
+	void Start () {
+		controladorOni = oni.GetComponent<ControladorOni>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	}
+}
