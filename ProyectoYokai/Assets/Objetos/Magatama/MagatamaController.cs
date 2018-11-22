67a6286e62cca7be1d74268404dc40db85644dac
acarcila <acarcila98@gmail.com>
2018-11-22 03:37:35 -0500

    Se agregaron las magatamas

---
+++ ProyectoYokai/Assets/Objetos/Magatama/MagatamaController.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MagatamaController : MonoBehaviour {
+
+	void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.gameObject.name == "Personaje")
+        {
+            GameManager gameManager = GameObject.FindGameObjectsWithTag("GameController")[0].GetComponent<GameManager>();
+			gameManager.aumentarMagatamas(1);
+            Destroy(this.gameObject);
+        }
+    }
+}
