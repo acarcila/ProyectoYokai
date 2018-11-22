14fa4e9c6af59b262d5374e845acf0c77f9744a5
acarcila <acarcila98@gmail.com>
2018-11-22 00:02:47 -0500

    Se agregó la barra de vida a los enemigos

---
+++ ProyectoYokai/Assets/Personajes/Enemigos/HealthBehaviour.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class HealthBehaviour : MonoBehaviour {
+
+	public Image imageVida;
+	public ControladorEnemigoVida controladorEnemigoVida;
+
+	private float vidaMaxima;
+
+	// Use this for initialization
+	void Start () {
+		vidaMaxima = controladorEnemigoVida.vida;
+	}
+	
+	
+	// Update is called once per frame
+	void Update () {
+		imageVida.fillAmount = controladorEnemigoVida.vida / vidaMaxima;
+	}
+}
