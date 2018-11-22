e399480e4168bc144f1a1d29d936791509128c3f
acarcila <acarcila98@gmail.com>
2018-11-22 03:39:33 -0500

    Se agregó el HUD

---
+++ ProyectoYokai/Assets/Objetos/HUD/UIController.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UIController : MonoBehaviour {
+
+	public Text textCantidadMagatamas;
+	private GameManager gameManager;
+
+	// Use this for initialization
+	void Start () {
+		gameManager = GameObject.FindGameObjectsWithTag("GameController")[0].GetComponent<GameManager>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		textCantidadMagatamas.text = gameManager.cantidadMagatamas + "";
+	}
+}
