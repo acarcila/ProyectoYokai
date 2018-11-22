315b44b0e7b968756f2debd931812d4e5eae6cc6
acarcila <acarcila98@gmail.com>
2018-11-22 03:41:32 -0500

    Se agregó un puente con el que el usuario cambiara de escena al segundo yokai

---
+++ ProyectoYokai/Assets/CambioEscena.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class CambioEscena : MonoBehaviour {
+
+	void OnTriggerEnter2D(Collider2D other)
+	{	
+		if(other.gameObject.name == "Personaje")
+		{
+			SceneManager.LoadScene("SegundoYokai", LoadSceneMode.Single);
+		}
+	}
+}
